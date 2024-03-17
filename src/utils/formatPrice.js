858dc22aa9a2f015559732e7d6ba020acd56215e
Diaa-E <blarb21@yahoo.com>
2024-03-17 05:29:25 +0200

    Split price into a whole and fraction segments

---
+++ src/utils/formatPrice.js
@@ -0,0 +1,9 @@
+export function formatPrice(price)
+{
+    price = price.toString();
+    const splitprice = price.split(".");
+
+    if (!splitprice[1]) return [splitprice[0], "00"];
+    if (+splitprice[1] < 10) return [splitprice[0], splitprice[1] + "0"];
+    return splitprice;
+}
