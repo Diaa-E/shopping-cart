541e66b688295da98827cd5e45207c1f9007dfa2
Diaa-E <blarb21@yahoo.com>
2024-03-18 02:12:39 +0200

    Write unit tests for sortByPrice function

--- src/utils/__tests__/productSorter.test.js
+++ src/utils/__tests__/productSorter.test.js
@@ -1,5 +1,83 @@
 import { describe, expect, it } from "vitest";
-import { sortByTitle } from "../productSorter";
+import { sortByPrice, sortByTitle } from "../productSorter";
+
+describe("sortByPrice utility function", () => {
+
+    it("Sorts lower to higher", () => {
+
+        expect(sortByPrice([
+            {
+                price: 12.4
+            },
+            {
+                price: 3.4
+            },
+            {
+                price: 2.25
+            }
+        ], false)).toEqual([
+            {
+                price: 2.25
+            },
+            {
+                price: 3.4
+            },
+            {
+                price: 12.4
+            },
+        ]);
+    }); 
+
+    it("Sorts higher to lower when passed true", () => {
+
+        expect(sortByPrice([
+            {
+                price: 12.4
+            },
+            {
+                price: 3.4
+            },
+            {
+                price: 2.25
+            }
+        ], true)).toEqual([
+            {
+                price: 12.4
+            },
+            {
+                price: 3.4
+            },
+            {
+                price: 2.25
+            },
+        ]);
+    }); 
+
+    it("Handles string input", () => {
+
+        expect(sortByPrice([
+            {
+                price: "12.4"
+            },
+            {
+                price: "3.4"
+            },
+            {
+                price: "2.25"
+            }
+        ], false)).toEqual([
+            {
+                price: "2.25"
+            },
+            {
+                price: "3.4"
+            },
+            {
+                price: "12.4"
+            },
+        ]);
+    }); 
+});
 
 describe("sortByTitle urility function", () => {
 
