2271b36ed7b0f0047e7c3ff0c8ae9a3878d2c4bb
Diaa-E <blarb21@yahoo.com>
2024-03-19 18:42:48 +0200

    Open links i nthe same tab to avoid spamming with empty links

--- src/components/FooterLink.jsx
+++ src/components/FooterLink.jsx
@@ -4,7 +4,7 @@ import styles from "../styles/FooterLink.module.css";
 export default function FooterLink({ text, to, icon })
 {
     return (
-        <Link target="_blank" className={styles["footer-link"]} to={to}>
+        <Link className={styles["footer-link"]} to={to}>
             {
                 icon &&
                 <img src={icon} alt={text + " link icon"} />
