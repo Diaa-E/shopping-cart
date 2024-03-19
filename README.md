f781f88b113475988b868ec56ed8b417627ad8b7
Diaa-E <blarb21@yahoo.com>
2024-03-19 19:27:47 +0200

    Add home page hero banner

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
-# React + Vite
+# Shopping Cart
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+A generic shopping cart
 
-Currently, two official plugins are available:
+## Credits
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+- [Image by Preis_King from Pixabay](https://pixabay.com/photos/online-shopping-amazon-shop-4532460/)
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,26 @@
+import RegularButton from "../components/RegularButton";
+import styles from "../styles/Home.module.css";
+
 export default function Home({})
 {
     return (
-        <h1>Home</h1>
+        <div className={styles["hero"]}>
+            <div className={styles["hero-banner"]}>
+                <h2>Qui ea atque minus sequi rem vitae dolorem, magnam saepe, debitis quisquam suscipit.</h2>
+                <h1>Lorem ipsum dolor sit amet consectetur</h1>
+                <RegularButton
+                    text={"Shop now"}
+                    classes={[styles["call-to-action"]]}
+                    style="primary"
+                    onClick={() => {}}
+                />
+                <RegularButton
+                    text={"Learn more"}
+                    classes={[styles["call-to-action"]]}
+                    style="secondary"
+                    onClick={() => {}}
+                />
+            </div>
+        </div>
     )
 }
