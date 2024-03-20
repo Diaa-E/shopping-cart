06b77f5baf3adc086618f3c7c7f5fa8627ddd656
Diaa-E <blarb21@yahoo.com>
2024-03-20 17:02:44 +0200

    Remove unused code

--- src/components/ShopItem.jsx
+++ src/components/ShopItem.jsx
@@ -1,13 +1,10 @@
 import { Link } from "react-router-dom";
 import styles from "../styles/ShopItem.module.css";
-import { formatPrice } from "../utils/formatPrice";
 import { appIcons } from "../data/appIcons.barrel";
 import PriceTag from "./PriceTag";
 
 export default function ShopItem({ image, title, price, id, inCart })
 {
-    const splitPrice = formatPrice(price);
-
     return (
         <Link to={"/shop/" + id} className={styles["shop-item"]}>
             <img src={image} alt={title + " image"} />
