8bcc66435a592c8ae23f5826f7f230f2f7d08774
Diaa-E <blarb21@yahoo.com>
2024-03-21 07:40:55 +0200

    Add styles to routing error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,14 +1,20 @@
 import { useRouteError } from "react-router";
 import { Link } from "react-router-dom";
+import errorLogo from "../assets/images/logo_error.svg";
+import styles from "../styles/Error.module.css";
 
 export default function Error({})
 {
     const error = useRouteError();
     console.error(error.status + ": " + error.statusText);
     return (
-        <>
-            <h1>{`Error: ${error.status}  (${error.statusText})`}</h1>
-            <Link to={"/"}>Home</Link>
-        </>
+        <div className={styles["error-container"]}>
+            <img className={styles["error-logo"]} src={errorLogo} alt="" />
+            <div className={styles["error-text"]}>
+                <h1 className={styles["error-number"]}>{error.status}</h1>
+                <h2 className={styles["error-title"]}>{error.statusText}</h2>
+            </div>
+            <Link className={styles["home-link"]} to={"/"}>Home</Link>
+        </div>
     )
 }
