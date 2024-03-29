dd0179989e04ab1e93d9b737548fb0636c12ec90
Diaa-E <blarb21@yahoo.com>
2024-03-29 18:05:59 +0200

    Fix mobile nav tests

--- src/components/__tests__/MobileNav.test.jsx
+++ src/components/__tests__/MobileNav.test.jsx
@@ -1,10 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
-import { render } from "@testing-library/react";
-import { describe, expect, it, vi } from "vitest";
+import { act, fireEvent, render} from "@testing-library/react";
+import { beforeEach, describe, expect, it, vi } from "vitest";
 import userEvent from "@testing-library/user-event";
 import MobileNav from "../MobileNav";
 
 describe("MobileNav component", () => {
+
+    beforeEach(() => vi.useFakeTimers({shouldAdvanceTime: true}));
+    afterEach(() => vi.resetAllMocks());
 
     function setup(jsx)
     {
@@ -14,69 +17,69 @@ describe("MobileNav component", () => {
         }
     }
 
-    it("Opens nav menu when nav button is clicked", async () => {
+    it("Opens nav menu when nav button is clicked", () => {
 
-        const { user, container} = setup(<MobileNav setLockScroll={() => {}}/>);
+        const {container} = setup(<MobileNav setLockScroll={() => {}}/>);
         const navMenuButton = container.querySelector("button#navMenuButton");
 
         expect(container.querySelector("nav#navMenu")).toBeNull();
 
-        await user.click(navMenuButton);
+        fireEvent.click(navMenuButton);
 
         expect(container.querySelector("nav#navMenu")).toBeInTheDocument();
     });
 
-    it("Locks scroll when nav menu is open", async () => {
+    it("Locks scroll when nav menu is open", () => {
 
         const setLockScroll = vi.fn();
-        const { user, container} = setup(<MobileNav setLockScroll={setLockScroll}/>);
+        const {container} = setup(<MobileNav setLockScroll={setLockScroll}/>);
         const navMenuButton = container.querySelector("button#navMenuButton");
-        await user.click(navMenuButton);
+        fireEvent.click(navMenuButton);
 
         expect(setLockScroll).toHaveBeenCalledWith(true);
     });
 
-    it("Closes nav menu when backdrop is clicked", async () => {
+    it("Closes nav menu when backdrop is clicked", () => {
 
-        const setLockScroll = vi.fn();
-        const { user, container} = setup(<MobileNav setLockScroll={setLockScroll}/>);
+        const {container} = setup(<MobileNav setLockScroll={() => {}}/>);
         const navMenuButton = container.querySelector("button#navMenuButton");
-        await user.click(navMenuButton);
+        fireEvent.click(navMenuButton);
         const backdropElement = container.querySelector("#navBackdrop");
         const navMenuElement = container.querySelector("nav#navMenu");
 
         expect(backdropElement).toBeInTheDocument();
 
-        await user.click(backdropElement);
+        fireEvent.click(backdropElement);
+        act(() => vi.runAllTimers());
 
         expect(navMenuElement).not.toBeInTheDocument();
     });
 
-    it("Closes nav menu when close nav menu button is clicked", async () => {
+    it("Closes nav menu when close nav menu button is clicked", () => {
 
-        const setLockScroll = vi.fn();
-        const { user, container} = setup(<MobileNav setLockScroll={setLockScroll}/>);
+        const {container} = setup(<MobileNav setLockScroll={(() => {})}/>);
         const navMenuButton = container.querySelector("button#navMenuButton");
-        await user.click(navMenuButton);
+        fireEvent.click(navMenuButton);
         const closeNavMenuButton = container.querySelector("button#closeNavMenu");
         const navMenuElement = container.querySelector("nav#navMenu");
 
         expect(closeNavMenuButton).toBeInTheDocument();
 
-        await user.click(closeNavMenuButton);
+        fireEvent.click(closeNavMenuButton);
+        act(() => vi.runAllTimers());
 
         expect(navMenuElement).not.toBeInTheDocument();
     });
 
-    it("Unlocks scroll when nav menu is closed", async () => {
+    it("Unlocks scroll when nav menu is closed", () => {
 
         const setLockScroll = vi.fn();
-        const { user, container} = setup(<MobileNav setLockScroll={setLockScroll}/>);
+        const {container} = setup(<MobileNav setLockScroll={setLockScroll}/>);
         const navMenuButton = container.querySelector("button#navMenuButton");
-        await user.click(navMenuButton);
+        fireEvent.click(navMenuButton);
         const closeNavMenuButton = container.querySelector("button#closeNavMenu");
-        await user.click(closeNavMenuButton);
+        act(() => fireEvent.click(closeNavMenuButton));
 
         expect(setLockScroll).toHaveBeenCalledWith(false);
-    })
+    });
 });
